921993a34b9e71a48b68d4162364bf86cb67ac49
RobertoAngelCetzalAlvarez <20212294@utcentro.onmicrosoft.com>
2023-02-21 22:42:32 -0600

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.main', ['class' => 'off-canvas-sidebar', 'activePage' => 'login', 'title' => __('Booking')])
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login prueba') }}</div>
+<div class="container" style="height: auto;">
+    <div class="row align-items-center">
+        <div class="col-md-9 ml-auto mr-auto mb-3 text-center">
+            <h3>{{ __('Log in here.') }}</h3>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                <div class="card card-login card-hidden mb-3">
+                    <div class="card-header card-header-primary text-center">
+                        <h4 class="card-title"><strong>{{ __('LoginENTRADA') }}</strong></h4>
+                        <div class="social-line">
+                            <a href="#" class="btn btn-just-icon btn-link btn-white">
+                                <i class="fa fa-facebook-square"></i>
+                            </a>
+                            <a href="#" class="btn btn-just-icon btn-link btn-white">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                            <a href="#" class="btn btn-just-icon btn-link btn-white">
+                                <i class="fa fa-google-plus"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <p class="card-description text-center">{{ __('Inserte sus credenciales') }}</p>
+                    <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">
+                            <span class="input-group-text">
+                                        <i class="material-icons">email</i>
+                                    </span>
+                            </label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                 name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,7 +46,11 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">
+                                    <span class="input-group-text">
+                                    <i class="material-icons">lock_outline</i>
+                                    </span>
+                            </label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -64,7 +88,7 @@
                                 @endif
                             </div>
                         </div>
-                    </form>
+                   
                 </div>
             </div>
         </div>
