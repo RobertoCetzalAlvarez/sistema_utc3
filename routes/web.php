9cd21de1c702ca834e00953db62470d7f131b376
RobertoAngelCetzalAlvarez <20212294@utcentro.onmicrosoft.com>
2023-02-23 17:45:02 -0600

    plantilla

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('plantilla', function () {
+    return view('plantilla');
+});
 
 Auth::routes();
 
